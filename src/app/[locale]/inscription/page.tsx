eaaf3c94d3005d9e46b303a3421adc94695fb04b
Ibrahim Diallo <ibdiallo.ca@gmail.com>
2026-03-09 10:16:57 -0400

    feat: integrate TikTok and Meta Pixel tracking into registration flow

--- src/app/[locale]/inscription/page.tsx
+++ src/app/[locale]/inscription/page.tsx
@@ -1,7 +1,15 @@
 "use client";
 
+import { MetaPixel } from "@/components/MetaPixel";
+import { TikTokPixel } from "@/components/TikTokPixel";
 import { ProviderRegisterForm } from "./components/ProviderRegisterForm";
 
 export default function LandingPage() {
-  return <ProviderRegisterForm />;
+  return (
+    <>
+      <TikTokPixel />
+      <MetaPixel />
+      <ProviderRegisterForm />
+    </>
+  );
 }
